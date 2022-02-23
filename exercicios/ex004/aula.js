7812458e7bec0581bb152b9f032eeeeed358d9a0
Leuza Lima <96353075+Leuza-dev@users.noreply.github.com>
2022-02-22 22:16:15 -0300

    treino js

---
+++ exercicios/ex004/aula.js
@@ -0,0 +1,20 @@
+
+/*variáveis
+let idade= 35;
+console.log(idade)
+let hora= 16.49;
+console.log(hora)
+let altura= 1.64;
+console.log(altura)
+let nome= "Leuza";
+console.log(nome)
+document.write(nome)
+let valorIngressoAdulto= 20 
+document.write(valorIngressoAdulto)*/
+//tipos primitivos
+let nome= 'Leuza'; //string
+let idade= 35; //number literal
+let estaAprovado= true; // boolean
+let sobrenome=undefined; //undefined
+let corSelecionado= null; //redefinir  um valor
+//tipagem dinâmica
